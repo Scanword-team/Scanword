a2d3e19ebda7f78a28a965b56694e2e7fa1ebba0
Andrey <kochetpwr@gmail.com>
2022-11-07 21:49:43 +0400

    added type, audio and image to Question

--- src/main/java/ru/scanword/domain/Question.java
+++ src/main/java/ru/scanword/domain/Question.java
@@ -21,6 +21,17 @@ public class Question {
     @Column(name = "question")
     private String question;
 
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "audio")
+    private String audio;
+
+    @Column(name = "image")
+    private String image;
+
+
+
 
 
 }
--- src/main/java/ru/scanword/dto/QuestionDTO.java
+++ src/main/java/ru/scanword/dto/QuestionDTO.java
@@ -3,11 +3,16 @@ package ru.scanword.dto;
 import lombok.Data;
 import ru.scanword.domain.Scanword;
 
+import javax.persistence.Column;
+
 @Data
 public class QuestionDTO {
 
     private Long id;
     private String answer;
     private String question;
+    private String type;
+    private String audio;
+    private String image;
 
 }
